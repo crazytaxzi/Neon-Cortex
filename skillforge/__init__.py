412a86cc443777aa2e83917175ea5485e7e9e479
Sentionce <crazytaxzi@gmail.com>
2026-08-04 04:03:33 -0700

    Export SkillForge public API

--- skillforge/__init__.py
+++ skillforge/__init__.py
@@ -1,1 +1,5 @@
-# Package init for skillforge
+"""Public API for the Neon Cortex SkillForge utility."""
+
+from .core import create_skill, slugify, validate_skill
+
+__all__ = ["create_skill", "slugify", "validate_skill"]
